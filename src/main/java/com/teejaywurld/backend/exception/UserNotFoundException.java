4f4dbe1a80514791bb7345a0975522ce3e3ae8ef
Adetunji Henry <teejaywurld@gmail.com>
2023-02-14 02:11:46 +0100

    initial commit

---
+++ src/main/java/com/teejaywurld/backend/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.teejaywurld.backend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(Long id){
+        super("Could not find user with id " + id);
+    }
+}
